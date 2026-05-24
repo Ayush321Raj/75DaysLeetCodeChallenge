80e014af9e2910f6f088cc8abdbe065299e4fdf4
Ayush Raj <121043933+Ayush321Raj@users.noreply.github.com>
2026-05-24 23:12:57 +0530

    Time: 3 ms (86.69%), Space: 52.7 MB (11.32%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,48 @@
+class Solution {
+
+    public int numIslands(char[][] grid) {
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int islands = 0;
+
+        for (int r = 0; r < rows; r++) {
+
+            for (int c = 0; c < cols; c++) {
+
+                if (grid[r][c] == '1') {
+
+                    islands++;
+
+                    dfs(grid, r, c);
+                }
+            }
+        }
+
+        return islands;
+    }
+
+    private void dfs(char[][] grid, int r, int c) {
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        // Boundary + water check
+        if (r < 0 || c < 0 ||
+            r >= rows || c >= cols ||
+            grid[r][c] == '0') {
+
+            return;
+        }
+
+        // Mark visited
+        grid[r][c] = '0';
+
+        // Explore 4 directions
+        dfs(grid, r + 1, c);
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r, c - 1);
+    }
+}
